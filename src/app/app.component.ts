4186e45279b907db13a8e08bccf5e743d7655f31
ekisam <104451325+ealejand1@users.noreply.github.com>
2024-10-03 01:26:10 +0200

    1 Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from "./componentes/cabecera/cabecera.component";
+import { CuerpoComponent } from "./componentes/cuerpo/cuerpo.component";
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceraComponent, CuerpoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
